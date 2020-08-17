468da2d64ec3c4b4fc4a90b85ca5ce8edff29f9a
lcc0637 <luo941300781>
2020-08-17 16:41:29 +0800

    lcc

--- src/main/java/com/ipincloud/iotbj/sys/controller/ExceptionController.java
+++ src/main/java/com/ipincloud/iotbj/sys/controller/ExceptionController.java
@@ -1,48 +1,49 @@
 package com.ipincloud.iotbj.sys.controller;
 
 
+import com.ipincloud.iotbj.sys.domain.ResponseBean;
+import com.ipincloud.iotbj.sys.messinfo.MessageInfo;
 import org.apache.shiro.ShiroException;
 import org.apache.shiro.authz.UnauthorizedException;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.ipincloud.iotbj.sys.messinfo.MessageInfo;
-import com.ipincloud.iotbj.sys.domain.ResponseBean;
+import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ExceptionController {
     private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     // 捕捉shiro的异常
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    //@ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(ShiroException.class)
     public ResponseBean handle401(ShiroException e) {
 
         String exMess = MessageInfo.exceptionInfo(e);
-        logger.debug("ShiroException:"+ exMess);
+        logger.debug("ShiroException:" + exMess);
 
-        return new ResponseBean(200,"FAILED", "用户未登录", null);
+        return new ResponseBean(200, "FAILED", "用户未登录", null);
     }
 
     // 捕捉UnauthorizedException
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    //@ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseBean handle401() {
         logger.debug("401:用户未登录.");
-        return new ResponseBean(200,"FAILED", "用户未登录.", null);
+        return new ResponseBean(200, "FAILED", "用户未登录.", null);
     }
 
     // 捕捉其他所有异常
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    //@ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseBean globalException(HttpServletRequest request, Throwable ex) {
-
         String exMess = ex.getMessage();
-        logger.debug("ShiroException:"+ exMess);
-        return new ResponseBean(getStatus(request).value(),"FAILED", exMess, null);
+        logger.debug("ShiroException:" + exMess);
+        return new ResponseBean(200, "FAILED", "操作失败:" + exMess, null);
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
